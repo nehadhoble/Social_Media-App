6674295eadee514bd5f3c237b669ebb5f83151eb
Neha Dhoble <neha@gmail.com>
2022-10-18 21:16:09 +0530

    Initial

---
+++ app/src/main/java/com/example/social_mediaapp/SignInActivity.kt
@@ -0,0 +1,11 @@
+package com.example.social_mediaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
