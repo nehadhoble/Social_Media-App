3739f7c4cfbf4a1391a28a49bb8a99dfcb40f3ed
Neha Dhoble <neha@gmail.com>
2022-10-19 21:38:59 +0530

    Initial

--- app/src/main/java/com/example/social_mediaapp/SignInActivity.kt
+++ app/src/main/java/com/example/social_mediaapp/SignInActivity.kt
@@ -2,10 +2,40 @@ package com.example.social_mediaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN: Int = 123
+    private val TAG = "SignInActivity Tag"
+    private lateinit var auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = Firebase.auth
+
+        signInButton.setOnClickListener {
+            signIn()
+        }
+    }
+
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 }
